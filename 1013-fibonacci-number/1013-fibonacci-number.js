6666e905d183928151af5ab035b5313c023be1f9
Menase Debel <143357007+menase7@users.noreply.github.com>
2025-01-09 20:00:58 +0300

    Time: 60 ms (46.9%), Space: 49 MB (22.44%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let array = [0,1];
+    for(let i=2; i<=n; i++){
+        array.push(array[i-1]+array[i-2]);
+    }
+    return array[n];
+};
