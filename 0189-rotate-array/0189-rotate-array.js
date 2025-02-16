ed64a6d24a2b879ce8514f29203312c0f8c4b21c
Menase Debel <143357007+menase7@users.noreply.github.com>
2025-02-16 23:50:46 +0300

    Time: 5 ms (21.65%), Space: 63.5 MB (6.18%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let n = nums.length;
+    k = k % n;  
+
+    const reverse = (start, end) => {
+        while (start < end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+            start++;
+            end--;
+        }
+    };
+
+    reverse(0, n - 1);
+
+    reverse(0, k - 1);
+
+    reverse(k, n - 1);
+};
